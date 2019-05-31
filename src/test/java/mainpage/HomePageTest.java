387141511fe1c4c6c74ea0d7985423dfa07df554
Quoc Nguyen 84 <quoc.nguyen@vn.devinition.com>
2019-06-01 00:55:43 +0700

    Click button show result

--- src/test/java/mainpage/HomePageTest.java
+++ src/test/java/mainpage/HomePageTest.java
@@ -2,7 +2,13 @@ package mainpage;
 
 import static org.testng.Assert.assertEquals;
 
+import java.io.File;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -12,15 +18,48 @@ public class HomePageTest {
 	public void testHomePage() throws InterruptedException {
 		System.out.println("Prepare Test");
 		System.setProperty("webdriver.chrome.driver",
-				"C:/Users/Quoc Nguyen/Documents/workspace-sts-3.8.2.RELEASE/gobear/driver/chromedriver.exe");
-
+				"./src/test/resources/driver/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("http://www.google.com");
+		System.out.println("Prepare navigating to gobear.com");
+		driver.get("https://www.gobear.com/ph?x_session_type=UAT");
+		Thread.sleep(5000);
+		System.out.println("At gobear.com");
+		WebElement btnShowResult = driver.findElement(By.name("product-form-submit"));
+		if (btnShowResult != null) {
+			btnShowResult.click();
+		}
+		
 		System.out.println("Before sleep");
 		Thread.sleep(10000);
 		System.out.println("After sleep");
 		driver.close();
-//		assertEquals("1", "2", "This one not equals");
 	}
+	
+	/**
+	 * 
+	 * This function will take screenshot
+	 * 
+	 * @param webdriver
+	 * 
+	 * @param fileWithPath
+	 * 
+	 * @throws Exception
+	 * 
+	 */
+
+//	public static void takeSnapShot(WebDriver webdriver, String fileWithPath) throws Exception {
+//
+//		// Convert web driver object to TakeScreenshot
+//		TakesScreenshot scrShot = ((TakesScreenshot) webdriver);
+//		// Call getScreenshotAs method to create image file
+//		File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
+//		// Move image file to new destination
+//		File DestFile = new File(fileWithPath);
+//
+//		// Copy file at destination
+//
+//		FileUtils.copyFile(SrcFile, DestFile);
+//
+//	}
 
 }
